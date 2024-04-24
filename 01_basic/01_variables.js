bca1ce8d378269218166cef8d97abf208513bd26
zahoorahmedsamani <91538508+zahoorahmedsamani@users.noreply.github.com>
2024-04-24 05:41:08 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123
+let accountName = "Ahmed"
+var accountEmail = "zahoor@gmail.com"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+accountName = "Samani"
+accountEmail = "samani@gmail.com"
+accountCity = "Pune"
+
+console.table([accountId, accountName, accountEmail, accountCity])
