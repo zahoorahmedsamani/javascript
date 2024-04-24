8f3bc6a8e98202690da2ba530770e4804a5c5a0e
zahoorahmedsamani <91538508+zahoorahmedsamani@users.noreply.github.com>
2024-04-24 06:09:17 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // use latest version of javascript
+
+// Primitive Datatype
+// number 
+// string
+// boolean
+// symbol
+// undefined 
+// null
+
+// object
+
+console.log(typeof "Ahmed");
+console.log(typeof 1233);
+console.log(typeof true);
+console.log(typeof undefined); // output - undefined
+console.log(typeof null); // output - object
