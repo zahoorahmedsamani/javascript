bcd7d1d955aa8bf1b499c86363e816e84e4b9f85
Zahoor Samani <zahoorahmedsamani@gmail.com>
2024-04-25 16:32:13 +0530

    comparision

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -10,8 +10,19 @@
 
 // object
 
-console.log(typeof "Ahmed");
-console.log(typeof 1233);
-console.log(typeof true);
-console.log(typeof undefined); // output - undefined
-console.log(typeof null); // output - object
+// console.log(typeof "Ahmed");
+// console.log(typeof 1233);
+// console.log(typeof true);
+// console.log(typeof undefined); // output - undefined
+// console.log(typeof null); // output - object
+const testName = "He58o World"
+console.log(testName.slice(4))
+
+
+let max= 1
+let min=1
+
+//random = 0.45389
+
+
+console.log(Math.floor(Math.random() * (max + min - 1)) )  // This is the formula used to get the value in desired range
---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,15 @@
+console.log(3 > 2);
+console.log(3 < 4);
+console.log(4 >= 5);
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null == 0);
+console.log(null === 0);
+
+console.log(undefined > 0);
+console.log(undefined <= 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
