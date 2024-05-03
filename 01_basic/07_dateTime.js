da6eea299170c6df0d1890fc94daf0f977bad975
Zahoor Samani <zahoorahmedsamani@gmail.com>
2024-05-03 12:00:20 +0530

    Date and time

---
+++ 01_basic/07_dateTime.js
@@ -0,0 +1,11 @@
+const newDate = new Date();
+
+console.log(newDate);
+console.log(newDate.toDateString());
+console.log(newDate.toLocaleString());
+console.log(newDate.toString());
+console.log(newDate.toISOString());
+console.log(newDate.getHours());
+
+
+
