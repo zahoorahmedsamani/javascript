dfbef30f0038dc43349cf10864ba5aae5f3f19d0
Zahoor Samani <zahoorahmedsamani@gmail.com>
2024-05-03 12:46:43 +0530

    Arrays

---
+++ 02_basic/01_array.js
@@ -0,0 +1,22 @@
+const newArray = new Array(1,2,3,4,5)
+
+const n1 = [1,2,3,4,5,6]
+
+// console.log(newArray);
+// console.log(n1);
+// Push and Pop method
+newArray.push(6) //Adding in the last
+newArray.pop() // Removing from last
+//console.log(newArray);
+
+// Unshift and Shift Method
+n1.unshift(9) //Add from Start
+n1.shift() // Remove from Start
+//console.log(n1);
+
+// Slice and Splice Method
+n1.slice(1, 3)
+n1.splice(0, 3)
+console.log(n1);
+
+
